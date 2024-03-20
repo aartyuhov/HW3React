9ac06d3aefefcd6c0d216f1e27f4cb82a481d4cf
aartyuhov <aartyuhov@gmail.com>
2024-03-20 22:32:32 +0200

    HW1React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+
+  const fetchQuoteOfTheDay = async () => {
+    try {
+      const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=happiness',{
+          headers:{
+            'X-Api-Key': '2zaXV2yJa2jY41+9oJveKg==8x1OmnZaTm5UkuPH'
+          }
+        });
+      console.log(response);
+      const { quote } = response.data[0];
+      console.log(quote);
+      setQuote(quote);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <button className="fetch-button" onClick={fetchQuoteOfTheDay}>
+        Fetch Quote of the Day
+      </button>
+      {quote && (
+        <div className="quote-container">
+          <p>"{quote}"</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
